f74bce3811c05d30210e396c96a272a41913575e
Ans-cmd336 <gangwaransh3546@gmail.com>
2025-10-23 09:22:42 +0530

    adding v1

---
+++ GodotGoogleLogin/src/main/java/com/ansh/godotgooglelogin/GoogleLogin.kt
@@ -0,0 +1,10 @@
+package com.ansh.godotgooglelogin
+
+import android.util.Log
+
+class GoogleLogin {
+    fun test(): String {
+        Log.d("GodotGoogleLogin", "Plugin loaded successfully")
+        return "GoogleLogin plugin working!"
+    }
+}
